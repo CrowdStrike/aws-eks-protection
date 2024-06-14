03614ac6d49684b4a53c47483aad87e3e62e72ba
Ryan Payne <ryanj.payne@outlook.com>
2024-06-14 09:14:48 -0500

    updates

--- lambda-source/existing-clusters/lambda_function.py
+++ lambda-source/existing-clusters/lambda_function.py
@@ -16,14 +16,14 @@ FAILED = "FAILED"
 DATE = date.today()
 PROJECT = os.environ['project_name']
 BUCKET = os.environ['artifact_bucket']
-region = os.environ['current_region']
+REGION = os.environ['AWS_DEFAULT_REGION']
 
-def start_build(region, clusterName, cluster_arn, node_type):
+def start_build(clusterName, cluster_arn, node_type):
     try:
         session = boto3.session.Session()
         client = session.client(
             service_name='codebuild',
-            region_name=region
+            region_name=REGION
         )
         build = client.start_build(
             projectName=PROJECT,
@@ -64,16 +64,39 @@ def check_fargate(region, clusterName):
         region_name=region
     )
     try:
-        client.list_fargate_profiles(
+        response = client.list_fargate_profiles(
             clusterName=clusterName,
             maxResults=10
         )
-        node_type = 'fargate'
+        if response['fargateProfileNames'] not in []:
+            logger.info('No fargate profiles found, setting node_type to nodegroup...')
+            node_type = 'nodegroup'
+            return node_type
+        else:
+            node_type = 'fargate'
+            return node_type
+    except botocore.exceptions.ClientError as error:
+        logger.error(error)
+        node_type = 'none'
         return node_type
-    except botocore.exceptions.ResourceNotFoundException:
-        logger.info('No fargate profiles found, setting node_type to nodegroup...')
-        node_type = 'nodegroup'
-        return node_type
+    
+def get_active_regions():
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='ec2',
+        region_name=REGION
+    )
+    try:
+        active_regions = []
+        describe_regions_response = client.describe_regions(AllRegions=False)
+        regions = describe_regions_response['Regions']
+        for region in regions:
+            active_regions += [region['RegionName']]
+        return active_regions
+    except botocore.exceptions.ClientError as error:
+        logger.error(error)
+        active_regions = []
+        return active_regions
     
 def cfnresponse_send(event, context, responseStatus, responseData, physicalResourceId=None, noEcho=False):
     responseUrl = event['ResponseURL']
@@ -108,24 +131,27 @@ def lambda_handler(event,context):
 
     logger.info('Checking EKS Cluster for API Access Config..')
     try:
-        session = boto3.session.Session()
-        client = session.client(
-            service_name='eks',
-            region_name=region
-        )
-        clusters = []
-        cluster_list = client.list_clusters()
-        clusters = cluster_list.get('clusters')
-        for clusterName in clusters:
-            cluster_details = client.describe_cluster(
-                name=clusterName
+        active_regions = get_active_regions()
+        for region in active_regions:
+            session = boto3.session.Session()
+            client = session.client(
+                service_name='eks',
+                region_name=region
             )
-            cluster_arn = cluster_details.get('cluster', {}).get('arn')
-            node_type = check_fargate(region, clusterName)
-            if 'API' in cluster_details.get('cluster', {}).get('accessConfig', {}).get('authenticationMode'):
-                start_build(region, clusterName, cluster_arn, node_type)
-            else:
-                logger.info(f'API Access not enabled on cluster {clusterName}')
+            clusters = []
+            cluster_list = client.list_clusters()
+            clusters = cluster_list.get('clusters')
+            for clusterName in clusters:
+                cluster_details = client.describe_cluster(
+                    name=clusterName
+                )
+                cluster_arn = cluster_details.get('cluster', {}).get('arn')
+                node_type = check_fargate(region, clusterName)
+                if node_type not in 'none':
+                    if 'API' in cluster_details.get('cluster', {}).get('accessConfig', {}).get('authenticationMode'):
+                        start_build(clusterName, cluster_arn, node_type)
+                    else:
+                        logger.info(f'API Access not enabled on cluster {clusterName}')
         cfnresponse_send(event, context, SUCCESS, response_d, "CustomResourcePhysicalID")
     except botocore.exceptions.ClientError as error:
         logger.error(error)
