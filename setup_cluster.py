4766668f897e098a62a660b507927d453e70e4ef
Ryan Payne <ryan.payne@crowdstrike.com>
2025-08-08 10:02:13 -0500

    update arn and handle open network config

--- setup_cluster.py
+++ setup_cluster.py
@@ -19,7 +19,7 @@ ACCOUNT_ID = os.environ['ACCOUNT_ID']
 SWITCH_ROLE = os.environ['SWITCH_ROLE']
 NAT_IP = os.environ['NAT_IP']
 TASK_ARN = os.environ['TASK_ARN']
-ACCESS_POLICY = 'arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy'
+PARTITION = os.environ['PARTITION']
 
 def check_cluster(eks):
     cluster_details = eks.describe_cluster(
@@ -37,7 +37,7 @@ def check_cluster(eks):
     
 def setup_cluster(eks, public_access_cidrs):
     if SCOPE == 'organization':
-        arn = SWITCH_ROLE
+        arn = f'arn:{PARTITION}:iam::{ACCOUNT_ID}:role/{SWITCH_ROLE}'
     else:
         arn = TASK_ARN
     try:
@@ -56,39 +56,44 @@ def setup_cluster(eks, public_access_cidrs):
             logger.error(error)
     try:
         logger.info(f'Adding access policy for {EKS_CLUSTER_NAME}')
+        access_policy = f'arn:{PARTITION}:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy'
         eks.associate_access_policy(
             clusterName=EKS_CLUSTER_NAME,
             principalArn=arn,
-            policyArn=ACCESS_POLICY,
+            policyArn=access_policy,
             accessScope={
                 'type': 'cluster'
             }
         )
     except botocore.exceptions.ClientError as error:
         logger.error(error)
-    try:
-        logger.info(f'Adding NAT IP for {EKS_CLUSTER_NAME}')
-        public_access_cidrs.append(f'{NAT_IP}/32')
-        response = eks.update_cluster_config(
-            name=EKS_CLUSTER_NAME,
-            resourcesVpcConfig={
-                'publicAccessCidrs': public_access_cidrs
-            }
-        )
-        update_id = response['update']['id']
-        update_response = eks.describe_update(
-            name=EKS_CLUSTER_NAME,
-            updateId=update_id
-        )
-        while update_response['update']['status'] in 'InProgress':
-            logger.info('waiting for update to complete...')
-            time.sleep(30)
+    # Check if cluster already allows access from anywhere
+    if '0.0.0.0/0' in public_access_cidrs:
+        logger.info(f'Cluster {EKS_CLUSTER_NAME} already allows access from 0.0.0.0/0, skipping NAT IP update')
+    else:
+        try:
+            logger.info(f'Adding NAT IP for {EKS_CLUSTER_NAME}')
+            public_access_cidrs.append(f'{NAT_IP}/32')
+            response = eks.update_cluster_config(
+                name=EKS_CLUSTER_NAME,
+                resourcesVpcConfig={
+                    'publicAccessCidrs': public_access_cidrs
+                }
+            )
+            update_id = response['update']['id']
             update_response = eks.describe_update(
                 name=EKS_CLUSTER_NAME,
                 updateId=update_id
             )
-    except botocore.exceptions.ClientError as error:
-        logger.error(error)
+            while update_response['update']['status'] in 'InProgress':
+                logger.info('waiting for update to complete...')
+                time.sleep(30)
+                update_response = eks.describe_update(
+                    name=EKS_CLUSTER_NAME,
+                    updateId=update_id
+                )
+        except botocore.exceptions.ClientError as error:
+            logger.error(error)
     logger.info(f'Cluster: {EKS_CLUSTER_NAME} is now setup')
     return
 
